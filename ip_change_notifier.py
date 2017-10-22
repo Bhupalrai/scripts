d968d267854a34e25e01d01bbd2f43d8d2b0048d
Bhupalrai <nuraeen.rai@gmail.com>
2017-10-22 23:27:48 +0545

    run as daemon

--- ip_change_notifier.py
+++ ip_change_notifier.py
@@ -1,66 +1,74 @@
 """
-Version 1.0.0
+Version 1.0.1
 Notify for change in public ip.
-Last updated on 14 Oct 2017.
-
-TO DO:
-- Notification mail sent verification
-- Maintain status file for last_ip
-	- Resolves issue due to server reboot
-- Handle exception, do not exit after error
-	- Run as daemon
+Last updated on 18 Oct 2017.
 
 """
 import smtplib
 import socket
 import sys
 import time
-from urllib2 import urlopen
+import getpass
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 from datetime import timedelta
+from requests import get
 
 #
 # Global variables, change as required
-g_ip_url = 'http://ip.42.pl/raw'
+g_ip_url = 'http://api.ipify.org/'
 g_gmail_smtp_adr = 'smtp.gmail.com'
 g_gmail_smpt_port = '465'
-g_curpublic_ip = '000.000.000.000'
-g_from_adr = 'sender_email@email.com'
-g_to_adrs  = ['reciever_email@gmail.com']
+g_status_file = '/tmp/.icn_sts_f.dat'
+g_default_public_ip = '000.000.000.000'
+g_from_adr = 'sender@email.com'
+g_password = None
+g_to_adrs = ['reciever@email.com']
+g_lastmailsent = True
+g_ip_get_str = 'get'
+g_ip_set_str = 'set'
+g_sleep_time = 15
 #
 # No modify zone frome here
+def getTimestamp():
+    return str(time.strftime("%Y-%m-%d %H:%M:%S "))
+
+
 def logErrAndExit(err):
     print "Eror: " + str(err)
-    print "Exiting"
-    sys.exit(1)
+    #print "Exiting"
+    #sys.exit(1)
 
 
 def verifyIPv4(ip):
-    try: 
+    try:
         socket.inet_aton(ip)
-        return ip
     except:
-        logErrAndExit("Got invalid ip " + ip)
+        logErrAndExit(getTimestamp() + "Got invalid ip " + ip)
+        return False
+    return True
 
 
 def getPublicIp():
-	"""Report back public ipv4 address
-	rtype: IPv4
-	"""
-	try:
-            my_ip = urlopen(g_ip_url).read()
-            my_ip = my_ip.strip()
-        except Exception, e:
-            print "Error while reading public ip"
-            logErrAndExit(e)
-        #
-        # validate and return
-        return verifyIPv4(my_ip)
+    """Report back public ipv4 address
+    rtype: IPv4
+    """
+    global g_ip_url
+    try:
+        my_ip = get(g_ip_url).text
+    except Exception, e:
+        print getTimestamp() + "Error while reading public ip"
+        logErrAndExit(e)
+        return None
+    #
+    # validate and return
+    if verifyIPv4(my_ip):
+        return my_ip
+    else:
+        return None
 
 
-def getSystemUptime(dum):
-    dum = dum # seems issue without parameter, may be timedelta issue
+def getSystemUptime():
     try:
         with open('/proc/uptime', 'r') as f:
             uptime_sec = float(f.readline().split()[0])
@@ -70,58 +78,107 @@ def getSystemUptime(dum):
     return str(uptime_str)
 
 
-def notifyNewIp(new_ip):
+def notifyNewIp(new_ip, last_known_ip):
+    global g_lastmailsent
     username = g_from_adr
-    password = 'pwd'
 
+    #
     # login with ssl
     try:
             server = smtplib.SMTP_SSL(g_gmail_smtp_adr, g_gmail_smpt_port)
-            server.login(username,password)
+            server.login(username, g_password)
     except Exception, e:
-            print "Error occured while connecting to smtp server"
+            print getTimestamp() + "Error occurred while connecting to smtp server"
             logErrAndExit(e)
+            return	
     email_msg = MIMEMultipart()
     email_msg['From'] = g_from_adr
-    email_msg['To'] =  ", ".join(g_to_adrs)
+    email_msg['To'] = ", ".join(g_to_adrs)
     email_msg['Subject'] = "Server Ip changed"
 
-    body = """
-            Public ip has changed:
-            Last IP: {0:s}
-            New IP: {1:s}
-            System is running for: {2:s}            
-            
-            Note:
-            This is an autometically triggered email and contains some confidential 
-            inforamtation. If you have received this message in error, please 
-            notify the sender and delete the message. 
-            
-            Thank you            
-            """.format(g_curpublic_ip, new_ip, getSystemUptime('dummy'))
+    body = ("\n"
+            "            Public ip has changed:\n"
+            "            Last IP: {0:s}\n"
+            "            New IP: {1:s}\n"
+            "            System is running for: {2:s}\n"
+            "\n"
+            "            Note:\n"
+            "            This is an automatically triggered email and contains some confidential information.\n"
+            "            If you have received this message in error, please notify the sender and delete the message.\n"
+            "\n"
+            "            Thank you\n"
+            "            ").format(last_known_ip, new_ip, getSystemUptime())
     email_msg.attach(MIMEText(body, 'plain'))
     text = email_msg.as_string()
-    server.sendmail(g_from_adr, g_to_adrs, text)
-    server.quit()
+    try:
+        server.sendmail(g_from_adr, g_to_adrs, text)
+        server.quit()
+    except Exception, err:
+        print getTimestamp() + 'Sending email failed.'
+        logErrAndExit(err)
+        g_lastmailsent = False
+        return
+    #
+    # update current ip
+    lastKnownIP(g_ip_set_str, new_ip)
+    g_lastmailsent = True
+
+
+def lastKnownIP(mode, new_ip=None):
+    if mode == g_ip_get_str:
+        try:
+            with open(g_status_file, 'r+') as sts_f:
+                ip = sts_f.readline().strip()
+                if not ip:
+                    return g_default_public_ip
+                if verifyIPv4(ip):
+                    return ip
+                else:
+                    return g_default_public_ip
+        except EnvironmentError, err:
+            print getTimestamp() + 'error reading status file ' + g_status_file
+            logErrAndExit(err)
+            return g_default_public_ip
+    elif mode == g_ip_set_str and new_ip:
+        try:
+            with open(g_status_file, 'w+') as sts_f:
+                sts_f.write(new_ip)
+                return True
+        except EnvironmentError, err:
+            print getTimestamp() + 'error writing to status file ' + g_status_file
+            logErrAndExit(err)
+            return False
+    else :
+        return g_default_public_ip
 
 
 def main():
-    _1st_loop_flag = True
+    global g_password
+    g_password = getpass.getpass('email password for ' + g_from_adr + ': ')
+
     while 1:
-        global g_curpublic_ip
         #
         # check new ip
+        last_known_ip = lastKnownIP(g_ip_get_str)
         new_ip = getPublicIp()
-        if new_ip == g_curpublic_ip:
-            time.sleep(3)
+        if not new_ip:
+            print getTimestamp() + 'Got empty public ip.'
+            print getTimestamp() + 'We will try again'
+            time.sleep(g_sleep_time)
+            continue
+        #
+        # try notifying new ip if failed last time
+        if not g_lastmailsent:
+            notifyNewIp(new_ip, last_known_ip)
+            time.sleep(g_sleep_time)
+            continue
+        if new_ip == last_known_ip:
+            time.sleep(g_sleep_time)
             continue
         #
         # notify new ip and update
-        if not _1st_loop_flag:
-            print "Got new IP: " + new_ip
-            notifyNewIp(new_ip)
-            _1st_loop_flag = False
-        g_curpublic_ip = new_ip
+        print getTimestamp() + "Got new IP: " + new_ip
+        notifyNewIp(new_ip, last_known_ip)
 
 if __name__ == "__main__":
     main()
